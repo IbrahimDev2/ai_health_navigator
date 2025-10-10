be52779a588bceed9487bf0e13f79aa8c181ff5b
info.ibrahim172 <info.ibrahim172@gmail.com>
2025-10-10 12:48:38 +0000

    h

---
+++ .next/server/app/api/diagnose/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/diagnose/route.js")
+R.c("server/chunks/[root-of-the-server]__e6982ed8._.js")
+R.c("server/chunks/[root-of-the-server]__703022d1._.js")
+R.m(66898)
+R.m(14428)
+module.exports=R.m(14428).exports
